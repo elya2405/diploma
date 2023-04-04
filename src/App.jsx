f2cb0aacd58f4bda39d2f868ffc4db5f10f27f20
kanybekova eleonora <eelya250@gmail.com>
2023-04-04 15:22:03 +0600

    formated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 export default function App() {
   return (
     <div className="App">
-    <h1>Hello world</h1>
-    <p>this is my diploma</p>
+      <h1>Hello world</h1>
+      <p>this is my diploma</p>
     </div>
   );
 }
