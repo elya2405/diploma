e82847fd3713c7896ff9e7be4f7e30c7ea5851c2
kanybekova eleonora <eelya250@gmail.com>
2023-04-06 15:06:36 +0600

    Added the delivery link

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -7,6 +7,7 @@ export default function Nav(props) {
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About</NavLink>
       <NavLink to="/contact">Contact</NavLink>
+      <NavLink to="/delivery">Delivery</NavLink>
     </nav>
   );
 }
