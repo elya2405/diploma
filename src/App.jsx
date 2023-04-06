a81eb9581919801c0b927917495d05f70063810f
kanybekova eleonora <eelya250@gmail.com>
2023-04-06 15:18:52 +0600

    Added the Home component with the content and formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
 export default function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path="/" element={<h2>Home</h2>} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<h2>About</h2>} />
           <Route path="/contact" element={<h2>Contact</h2>} />
           <Route path="/delivery" element={<h2>Delivery</h2>} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcom to my Sportwear store.</h1>
+      <p>This is my very aestetic shop.</p>
+    </div>
+  );
+}
