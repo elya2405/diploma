79709f63a8240ccc721608efe9257ad737eb5702
kanybekova eleonora <eelya250@gmail.com>
2023-04-11 15:39:05 +0600

    added category page

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "./CategoryList.css";
 
 export default function CategoryList() {
+  const [categories, setCategories] = useState([]);
+
+  const output = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
+    </li>
+  ));
   return (
     <div className="CategoryList">
-      <ul>
-
-      </ul>
+      <ul></ul>
     </div>
   );
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -10,7 +10,6 @@ export default function Layout(props) {
         <Logo />
         <Nav />
       </header>
-      <aside>ASIDE</aside>
       <main>{props.children}</main>
       <aside>
         <CategoryList />
