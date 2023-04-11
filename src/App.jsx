39077753d648e6f8b573acbb66fd08e7f43a1cb8
kanybekova eleonora <eelya250@gmail.com>
2023-04-11 15:03:26 +0600

    added a Сategory file and imported into the App and created a path to the Сategory

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Delivery from "./pages/Delivery";
+import Category from "./pages/Category";
 export default function App() {
   return (
     <div className="App">
@@ -13,7 +14,7 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/delivery" element={<Delivery />} />
-          <Route path="/category/:path" element={<></>} />
+          <Route path="/category/:path" element={<Category />} />
         </Routes>
       </Layout>
     </div>
---
+++ src/pages/Category.jsx
@@ -0,0 +1,7 @@
+export default function Category() {
+  returnv(
+    <div className="Category">
+      <h1>Category</h1>
+    </div>
+  );
+}
