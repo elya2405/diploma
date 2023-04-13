1b0bec04c0bad814cc28f719b1652a2ff95b8b6c
kanybekova eleonora <eelya250@gmail.com>
2023-04-13 14:23:40 +0600

    framed the layout with a new provider class

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,49 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Delivery from "./pages/Delivery";
 import Category from "./pages/Category";
+import { createContext, useEffect, useState } from "react";
+import { getDocs } from "firebase/firestore";
+import { categoryCollection } from "./firebase";
+
+export const AppContext = createContext({
+  categories: [],
+});
+
 export default function App() {
+  const [categories, setCategories] = useState([]);
+  //выполнить эту функцию только один раз
+  useEffect(() => {
+    //получить категории из списка категорий
+    getDocs(categoryCollection).then((snapshot) => {
+      //категории будут хранится в snapshot.docs
+
+      //создать массив для категорий
+      const newCategories = [];
+      //заполнить массив данными из списка категорий
+      snapshot.docs.forEach((doc) => {
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+      //задать новый массив как состояние компонента
+      setCategories(newCategories);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/delivery" element={<Delivery />} />
-          <Route path="/category/:path" element={<Category />} />
-        </Routes>
-      </Layout>
+      <AppContext.Provider value={{ categories }}>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/delivery" element={<Delivery />} />
+            <Route path="/category/:path" element={<Category />} />
+          </Routes>
+        </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
