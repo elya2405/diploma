a5373eb752619228eff641222fd869885a7891ba
kanybekova eleonora <eelya250@gmail.com>
2023-04-13 14:25:55 +0600

    cut the useState from the Categorylist and inserted it into the App

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,31 +1,12 @@
 import { getDoc, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
 import { categoryCollection } from "../../firebase";
 import "./CategoryList.css";
 
 export default function CategoryList() {
-  const [categories, setCategories] = useState([]);
-
-  //выполнить эту функцию только один раз
-  useEffect(() => {
-    //получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      //категории будут хранится в snapshot.docs
-
-      //создать массив для категорий
-      const newCategories = [];
-      //заполнить массив данными из списка категорий
-      snapshot.docs.forEach((doc) => {
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-      //задать новый массив как состояние компонента
-      setCategories(newCategories);
-    });
-  }, []);
+  const { categories } = useContext(AppContext);
 
   const output = categories.map((category) => (
     <li key={category.id}>
