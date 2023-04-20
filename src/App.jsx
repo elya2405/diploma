37955793973177868a4d0bc73e4ba5dc4e6e883f
kanybekova eleonora <eelya250@gmail.com>
2023-04-20 15:13:37 +0600

    save the contents of the shopping cart in the browser memory

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Home from "./pages/Home";
 import Delivery from "./pages/Delivery";
 import Category from "./pages/Category";
 import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
 import { categoryCollection, productCollection } from "./firebase";
 import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import Product from "./pages/Product";
+
 
 export const AppContext = createContext({
   categories: [],
@@ -16,49 +19,68 @@ export const AppContext = createContext({
 
   //корзина
   cart: {},
-  setCart: () => {},
+  setCart: () => { }
 });
 
 export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
 
+
   //корзина
-  const [cart, setCart] = useState({});
-  //выполнить эту функцию только один раз
+  const [cart, setCart] = useState(() => {
+    //восстанавливает содержимое корзинки из памяти брауизера
+    return JSON.parse(localStorage.getItem("cart")) || {};
+  });
+
+
+  //выполнить эту функцию только когда содержимое корзинки меняетсяя 
   useEffect(() => {
-    //получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      //категории будут хранится в snapshot.docs
+    //сохранить содержимое корзинки в памяти браузера
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
-      //создать массив для категорий
-      const newCategories = [];
-      //заполнить массив данными из списка категорий
-      snapshot.docs.forEach((doc) => {
-        const category = doc.data();
-        category.id = doc.id;
 
-        newCategories.push(category);
-      });
-      //задать новый массив как состояние компонента
-      setCategories(newCategories);
-    });
-    getDocs(productCollection).then((snapshot) => {
-      //продукты будут хранится в snapshot.docs
 
-      //создать массив для продуктов
-      const newProducts = [];
-      //заполнить массив данными из списка продуктов
-      snapshot.docs.forEach((doc) => {
-        const product = doc.data();
-        product.id = doc.id;
 
-        newProducts.push(product);
-      });
-      //задать новый массив как состояние компонента
-      setProducts(newProducts);
-    });
+  //выполнить эту функциу один раз
+  useEffect(() => {
+    //получить категроии из списка категорий
+    getDocs(categoryCollection)
+      .then(snapshot => {
+        //категории будут храниться в snapshot.docs
+        //слздать массив для категорий
+        const newCategories = [];
+        //заполнить массив данными из списка категорий
+        snapshot.docs.forEach(doc => {// doc это категория 
+          const category = doc.data();
+          category.id = doc.id;
+
+          newCategories.push(category);
+        });
+        //задать новый массив как состояние компонента
+        setCategories(newCategories);
+      })
+
+    //получить продукты из списка родукто
+    getDocs(productCollection)
+      .then(snapshot => {
+        //категории будут храниться в snapshot.docs
+        //создать массив для продуктов
+        const newProducts = [];
+        //заполнить массив данными из списка продуктов
+        snapshot.docs.forEach(doc => {// doc это категория 
+          const product = doc.data();
+          product.id = doc.id;
+
+          newProducts.push(product);
+        });
+        //задать новый массив как состояние компонента
+        setProducts(newProducts);
+      })
   }, []);
+
+
 
   return (
     <div className="App">
@@ -67,13 +89,19 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
+            <Route path="/contacts" element={<Contact />} />
             <Route path="/delivery" element={<Delivery />} />
             <Route path="/category/:path" element={<Category />} />
-            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/product/:path" element={<Product/>}/>
+            <Route path="/cart" element={<Cart />} />
+
+
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
       </AppContext.Provider>
+
+
     </div>
   );
 }
