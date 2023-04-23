8a0bedfa93710219f1f42a691cccde29f44ccc3f
kanybekova eleonora <eelya250@gmail.com>
2023-04-23 22:23:49 +0600

    swapped the price and the name of the product in the ProductList

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -3,21 +3,22 @@ import { Link } from "react-router-dom";
 import { AppContext } from "../../App";
 import AddToCart from "../AddToCart/AddToCart";
 import "./ProductList.css";
-export default function ProductList({category}) {
+export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-
   const output = products
-  .filter(product => product.category === category.id)
-  .map((product) => (
-    <div className="Product" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <div className="Product-content">
-      <Link className="Product-name" to={"/product/" + product.path}>{product.name}</Link>
-      <span>{product.price} $</span>
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div className="Product" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <div className="Product-content">
+          <span>{product.price} $</span>
+          <Link className="Product-name" to={"/product/" + product.path}>
+            {product.name}
+          </Link>
+        </div>
+        <AddToCart product={product} />
       </div>
-      <AddToCart product={product}/>
-    </div>
-  ));
+    ));
   return <div className="ProductList">{output}</div>;
 }
