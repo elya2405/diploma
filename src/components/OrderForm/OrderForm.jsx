54e517317b99499fbac7eb32dff738f6bbd703e6
kanybekova eleonora <eelya250@gmail.com>
2023-04-30 00:03:01 +0600

    stylized input in OrderForm

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -42,13 +42,13 @@ export default function OrderForm() {
       <form onSubmit={onSubmit}>
         <h3>Create an order</h3>
         <label>
-          Name: <input type="text" name="name" required />
+          <input placeholder="Name" type="text" name="name" required />
         </label>
         <label>
-          Phone: <input type="telephone" name="phone" required />
+          <input placeholder="Phone" type="telephone" name="phone" required />
         </label>
         <label>
-          Address: <input type="text" name="address" required />
+          <input placeholder=" Address" type="text" name="address" required />
         </label>
         <button>Submit</button>
       </form>
