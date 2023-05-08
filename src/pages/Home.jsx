8735c717cd99e45293f6badf583d51113cab68bc
kanybekova eleonora <eelya250@gmail.com>
2023-05-08 21:09:04 +0600

    moved aside with CategoryList to Home and imported it

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import CategoryList from "../components/CategoryList/CategoryList";
+
 export default function Home() {
   return (
     <div className="Home">
+      <aside>
+        <CategoryList />
+      </aside>
       <h1>Welcom to my Sportwear store.</h1>
       <p>This is my very aestetic shop.</p>
     </div>
