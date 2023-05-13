421c3d2145eb3a1f16c25174a973b3f399baa94a
kanybekova eleonora <eelya250@gmail.com>
2023-05-13 20:41:43 +0600

    fixed the error with the icon in Auth, imported it and then added the icon

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,6 +3,7 @@ import "./Auth.css";
 import { AppContext } from "../../App";
 import { logIn, logOut } from "../../firebase";
 import { Link } from "react-router-dom";
+import icon from "/src/assets/sign-in-icon.png";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
@@ -11,11 +12,7 @@ export default function Auth() {
   let output = (
     <span>
       <div className="SignIn">
-        <img
-          onClick={logIn}
-          className="SignInIcon"
-          src="/src/assets/sign-in-icon.png"
-        />
+        <img onClick={logIn} className="SignInIcon" src={icon} />
         <a href="#" onClick={logIn}>
           Sign in
         </a>
