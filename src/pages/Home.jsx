de3613955195cc2491ae016a50ec113c425d95e4
kanybekova eleonora <eelya250@gmail.com>
2023-05-14 01:17:00 +0600

    added three cards to Home with the title and content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import CategoryList from "../components/CategoryList/CategoryList";
 import "./Home.css";
 import image from "../assets/header-background.png";
+import image_for_cart_1 from "../assets/cart_1_img.jpg";
+import image_for_cart_2 from "../assets/cart_2_img.jpg";
+import image_for_cart_3 from "../assets/cart_3_img.jpg";
 
 export default function Home() {
   return (
@@ -12,13 +15,19 @@ export default function Home() {
       <aside>
         <CategoryList />
       </aside>
-
-      <div className="Cart1">
-        <div className="CartContent">
-          <h1>Your body deserves to look good</h1>
+      <h1>Here you will find</h1>
+      <div className="Carts">
+        <div className="Cart_1">
+          <h3>comfort</h3>
+          <img src={image_for_cart_1} />
         </div>
-        <div className="CartImg">
-          <img src="/src/assets/image-for-home1.png" />
+        <div className="Cart_2">
+          <h3>style</h3>
+          <img src={image_for_cart_2} />
+        </div>
+        <div className="Cart_3">
+          <h3>large selection</h3>
+          <img src={image_for_cart_3} />
         </div>
       </div>
     </div>
