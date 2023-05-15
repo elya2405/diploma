5909fd09a8b308c3f043b48ce60d4e669e578e4c
kanybekova eleonora <eelya250@gmail.com>
2023-05-15 19:16:53 +0600

    added an image for header in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,8 @@
+import image_for_about_header from "../assets/image_for_about_header.jpg";
 export default function About() {
   return (
     <div className="About">
-      <p>We've opened up</p>
+      <img className="Image_for_about_header" src={image_for_about_header} />
     </div>
   );
 }
