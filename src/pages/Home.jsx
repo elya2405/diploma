26096496da136414c35656f4aff7bdd6d4d7683e
kanybekova eleonora <eelya250@gmail.com>
2023-05-19 14:11:26 +0600

    stylized Home.jsx, made imports, removed unnecessary inscriptions and stylized pictures

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,45 +1,48 @@
 import CategoryList from "../components/CategoryList/CategoryList";
 import "./Home.css";
-import image from "../assets/image_for_home_header.png";
+import image_for_home_header from "../assets/image_for_home_header.png";
 import image_for_cart_1 from "../assets/cart_1_img.jpg";
 import image_for_cart_2 from "../assets/cart_2_img.jpg";
 import image_for_cart_3 from "../assets/cart_3_img.jpg";
-import image_for_home_1 from "../assets/image_for_home_1.png";
+
 
 export default function Home() {
   return (
     <div className="Home">
-      <img className="Image_for_home_header" src={image} />
-      <div className="FirstSlogan">
-        <h1>Sportswear that never goes out of fashion</h1>
-      </div>
+      <img className="Image_for_home_header" src={image_for_home_header} />
+      
       <aside>
         <CategoryList />
       </aside>
+      
       <section className="Section_1">
-        <h1>Here you will find</h1>
+      <h1>Here you will find</h1>
         <div className="Carts">
+        
           <div className="Cart_1">
+            
+            <img src={image_for_cart_1} />
             <div className="Cart_1_content">
               <h3>Comfort</h3>
               <p>Comfort in every movement</p>
             </div>
-            <img src={image_for_cart_1} />
           </div>
           <div className="Cart_2">
+            
+            <img src={image_for_cart_2} />
             <div className="Cart_1_content">
               <h3>Style</h3>
               <p>A style that you will fall in love with</p>
             </div>
-            <img src={image_for_cart_2} />
           </div>
           <div className="Cart_3">
+            
+
+            <img src={image_for_cart_3} />
             <div className="Cart_1_content">
               <h3>Large selection</h3>
               <p>You will enjoy shopping with us</p>
             </div>
-
-            <img src={image_for_cart_3} />
           </div>
         </div>
       </section>
