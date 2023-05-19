cfbce40edb3af03e6c0ca7b8b758da120e189f0f
kanybekova eleonora <eelya250@gmail.com>
2023-05-19 14:22:29 +0600

    added code that outputs to ProductList, CategoryList without pictures

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
 import AddProduct from "../AddProduct/AddProduct";
 import AddToCart from "../AddToCart/AddToCart";
@@ -7,26 +7,44 @@ import DeleteProduct from "../DeleteProduct/DeleteProduct";
 import "./ProductList.css";
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
+  const { categories } = useContext(AppContext);
 
   const output = products
     .filter((product) => product.category === category.id)
     .map((product) => (
       <div className="Product" key={product.id}>
-        <img src={product.picture} alt={product.name} />
-        <div className="Product-content">
-          <span>{product.price} $</span>
-          <Link className="Product-name" to={"/product/" + product.path}>
-            {product.name}
+        <div className="Product_content">
+          <Link  to={"/product/" + product.path}>
+           <div className="Product_link"><span className="Product_name"> {product.name}</span>
+           <img
+              className="Product_img"
+              src={product.picture}
+              alt={product.name}
+            /></div>
           </Link>
+          <span>{product.price} $ </span>
           <AddToCart className="Addtocard" product={product} />
-          <DeleteProduct product={product}/>
+          <DeleteProduct product={product} />
         </div>
       </div>
     ));
+  const output_2 = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={`/category/${category.path}`}>
+        <span>{category.name}</span>
+      </NavLink>
+    </li>
+  ));
+
   return (
-    <div className="ProductList">
-      {output}
-      <AddProduct category={category} />
+    <div>
+      <div>
+        <ul className="Category_name">{output_2}</ul>
+      </div>
+      <div className="ProductList">
+        {output}
+        <AddProduct category={category} />
+      </div>
     </div>
   );
 }
