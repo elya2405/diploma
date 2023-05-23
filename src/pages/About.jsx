1cf5378db7e233b6d2951e6f7b2a2faba1410b49
kanybekova eleonora <eelya250@gmail.com>
2023-05-23 09:10:18 +0600

    added a video with a paragraph in About, added classes for styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,29 @@
 import image_for_about_header from "../assets/image_for_about_header.jpg";
+import "./About.css";
 export default function About() {
   return (
     <div className="About">
       <img className="Image_for_about_header" src={image_for_about_header} />
+
+      <div className="Video_for_about">
+        <div>
+          <video className="Video_media" autoPlay muted loop>
+            <source
+              src="src/assets/video_2023-05-22_20-20-57.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+
+        <div className="Welcome">
+          <p>
+            Welcome to the "Sportwear store" - your ideal destination for
+            fashionable and functional sportswear. We are a premium store
+            offering a wide selection of quality products for an active
+            lifestyle.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
