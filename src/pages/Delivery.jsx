f7fb14b40117f3adbcf4db0e4dd4971e71fabbc0
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 03:22:18 +0600

    added a picture for the header background to Delivery, imported it

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -1,7 +1,12 @@
+import image_for_delivery_header from "../assets/image_for_delivery_header.png";
 export default function Delivery() {
   return (
     <div className="Delivery">
-      <p>Within 5 days</p>
+      <img
+        className="Image_for_delivery_header"
+        src={image_for_delivery_header}
+        alt=""
+      />
     </div>
   );
 }
