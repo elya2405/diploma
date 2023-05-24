05e3009001ca131e4b754e86a7e9b60bd5996b4f
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 00:06:36 +0600

    added footer stylized it, added svg icons and stylized lists, made it clickable

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
   return (
-    <footer className="Footer">
+    <footer className="Footer ">
       <div className="Footer_">
         <div className="icons">
           <a
@@ -27,11 +27,7 @@ function Footer() {
             </svg>
           </a>
 
-          <a
-            href="https://twitter.com/PUMA"
-            target="_blank"
-            rel="noreferrer"
-          >
+          <a href="https://twitter.com/PUMA" target="_blank" rel="noreferrer">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="20"
@@ -50,8 +46,8 @@ function Footer() {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
+              width="30"
+              height="30"
               fill="currentColor"
               className="icon"
               viewBox="0 0 16 16"
@@ -66,8 +62,8 @@ function Footer() {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
+              width="30"
+              height="30"
               fill="currentColor"
               class="icon"
               viewBox="0 0 16 16"
@@ -77,34 +73,113 @@ function Footer() {
           </a>
         </div>
       </div>
-      <div className="footer-container">
+      <div className="Footer_content">
         <div className="Explore">
-          <h3>Explore</h3>
-          <a href="https://kyliecosmetics.com/pages/contact">Home</a>
-          <a href="https://kyliecosmetics.com/pages/faq">About</a>
-          <a href="https://kyliecosmetics.com/pages/shipping">Contact</a>
-          <a href="https://kyliecosmetics.com/pages/order-tracking">Delivery</a>
+          <ul>
+            <h3>Explore</h3>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+            <li>
+              <a href="#">Delivery</a>
+            </li>
+          </ul>
         </div>
-        <div className="privacy">
-          <a href="https://kyliecosmetics.com/pages/privacy-policy">
-            privacy policy
-          </a>
-          <a href="https://kyliecosmetics.com/pages/terms">terms</a>
-          <a href="https://kyliecosmetics.com/pages/accessibility">
-            accessibility
-          </a>
-          <a href="https://kyliecosmetics.com/pages/accessibility#">
-            cookie settings
-          </a>
-          <a href="https://kyliecosmetics.com/pages/cookie-policy">
-            cookie policy
-          </a>
+        <div className="Categoryy">
+          <ul>
+            <h3>Category</h3>
+            <li>
+              <a href="#">Women</a>
+            </li>
+            <li>
+              <a href="#">Men</a>
+            </li>
+            <li>
+              <a href="#">Kids</a>
+            </li>
+            <li>
+              <a href="#">Best sellers</a>
+            </li>
+          </ul>
         </div>
-        <p className="coty">© 2023 coty operations</p>
+        <div className="Contactt">
+          <ul>
+            <h3>Contact</h3>
+            <li>
+              <a target="_blank" rel="noreferrer" href="tel:+996 700 133 094">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-telephone-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
+                  />
+                </svg>
+                Phone Number: +996 700 133 094
+              </a>
+            </li>
+            <li>
+              <a href="https://mail.google.com/mail/u/0/#inbox">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-envelope-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
+                </svg>
+                Email: ela376912@gmail.com
+              </a>
+            </li>
+            <li>
+              <a href="https://www.google.com/maps/place/Каракол/@42.4862583,78.3611002,13z/data=!3m1!4b1!4m6!3m5!1s0x38865b91e1f04c0b:0xeb5bfe3900e7ecfa!8m2!3d42.4782102!4d78.3955986!16zL20vMDFncWJk">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  class="bi bi-geo-alt-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
+                </svg>
+                Address: Kyrgyzstan, Karakol, Kashka-Suu micro district
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="Footer_end">
+        <div className="Footer_span">
+          <strong>All rights reserved</strong> © PUMA SE 2023
+        </div>
 
         <div className="Up">
           <Link href="#" onClick={scrollToTop}>
-            <img className="Up_icon" src={up_icon} /> <h3>GO UP</h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="30"
+              height="30"
+              fill="currentColor"
+              class="bi bi-arrow-up-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+            </svg>
           </Link>
         </div>
       </div>
