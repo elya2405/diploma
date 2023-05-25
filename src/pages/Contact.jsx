1e5e590892129eee578136ae54acd0342c4872db
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 14:12:15 +0600

    added 2 blocks to Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import image_for_contact_header from "../assets/image_for_contact_header.png";
+import "./Contact.css";
 export default function Contact() {
   return (
     <div className="Contact">
@@ -7,6 +8,28 @@ export default function Contact() {
         src={image_for_contact_header}
         alt=""
       />
+      <div className="Contacttt">
+      <div className="block_1">
+          <div className="block_1_content">
+            <h2>Contacts</h2>
+            <p>Электронная почта: info@sportwearstore.com</p>
+            
+            <p>Address: Street, City, Country, Postal Code</p>
+            <p>Phone: +X XXX XXX-XXXX</p>
+          </div>
+        </div>
+
+        <div className="block_2">
+          <div className="block_2_content">
+            <h2>Working hours:</h2>
+            <p>Monday - Friday: 9:00 - 18:00</p>
+            <p>Saturday: 10:00 - 15:00</p>
+            <p>Sunday: Closed</p>
+          </div>
+        </div>
+      </div>
+        
+      
     </div>
   );
 }
