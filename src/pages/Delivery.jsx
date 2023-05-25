8311e6cb4dcb1945e099fffa7ffa401d2bf0fbd9
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 14:27:08 +0600

    added content to Delivery

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -7,6 +7,26 @@ export default function Delivery() {
         src={image_for_delivery_header}
         alt=""
       />
+      <div className="Deliveryy">
+        <div className="methods">
+          <h4>Shipping methods:</h4>
+          <p>
+            Courier Delivery: Fast and convenient delivery directly to your
+            doorway.
+          </p>
+          <p>
+            Pick-up points: The possibility of pickup in a convenient place for
+            you.
+          </p>
+          <p>
+            Postal delivery: Sending orders by postal services for remote
+            regions.
+          </p>
+        </div>
+        <div className="">
+          
+        </div>
+      </div>
     </div>
   );
 }
