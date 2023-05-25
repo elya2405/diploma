ccae87b58cf36f58d305193d88a9f3e135ddff76
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 17:06:26 +0600

    fixed a bug

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,7 +3,7 @@ import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
 import AddToCart from "../components/AddToCart/AddToCart";
-import "./Product.css"
+import "./Product2.css";
 
 export default function Product() {
   const { params } = useMatch("/product/:path");
@@ -20,16 +20,16 @@ export default function Product() {
       <h1>{product.name}</h1>
       <div className="display_product">
         <img src={product.picture} alt={product.name} />
-        <div className="display_product_2">
+       
         <p>
           We offer a wide range of high-quality sportswear created for an active
           lifestyle. Our sportswear combines stylish design, comfort and
           functionality to help you achieve the best results in training and
           feel confident throughout the day.
         </p>
-        <span>{product.price} som</span>
+        <span>{product.price} $</span>
         <AddToCart product={product} />
-        </div>
+        
         
       </div>
     </div>
