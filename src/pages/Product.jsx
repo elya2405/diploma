a04b2412361f6354a3225e48f4e42b16d506aa29
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 15:41:11 +0600

    stylized Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,12 +3,13 @@ import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
 import AddToCart from "../components/AddToCart/AddToCart";
+import "./Product.css";
 
 export default function Product() {
   const { params } = useMatch("/product/:path");
-  const { products } = useContext(AppContext); 
+  const { products } = useContext(AppContext);
 
-  const product = products.find(product => product.path === params.path);
+  const product = products.find((product) => product.path === params.path);
 
   if (!product) {
     return <NotFound />;
@@ -17,10 +18,20 @@ export default function Product() {
   return (
     <div className="Product">
       <h1>{product.name}</h1>
-      <img src={product.picture} alt={product.name} />
-      <p>{product.description}</p>
-      <span>{product.price} som</span>
-      <AddToCart product={product} />
+      <div className="display_product">
+        <img src={product.picture} alt={product.name} />
+        <div className="display_product_2">
+        <p>
+          We offer a wide range of high-quality sportswear created for an active
+          lifestyle. Our sportswear combines stylish design, comfort and
+          functionality to help you achieve the best results in training and
+          feel confident throughout the day.
+        </p>
+        <span>{product.price} som</span>
+        <AddToCart product={product} />
+        </div>
+        
+      </div>
     </div>
   );
 }
