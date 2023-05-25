bc41f1113fb93d160930e840a28eaf86ffa57b0b
kanybekova eleonora <eelya250@gmail.com>
2023-05-25 16:45:47 +0600

    am correcting a mistake with a video

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,7 +3,7 @@ import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import NotFound from "./NotFound";
 import AddToCart from "../components/AddToCart/AddToCart";
-import "./Product.css";
+import "./Product.css"
 
 export default function Product() {
   const { params } = useMatch("/product/:path");
